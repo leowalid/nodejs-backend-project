a883136ea64da4a995cf7750e3362bed3e336a9f
Ahmed Ibrahim <ahmedwalied039@gmail.com>
2025-03-26 03:00:45 +0100

    Added basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Server is running on port ${port}`);
+    } else {
+        console.log(error);
+    }
+});
+
+
